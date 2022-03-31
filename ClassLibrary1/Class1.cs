62870bfbb442f95d5f5105595a88f3bb074d759b
hristian-carabulea <hristian.carabulea@outlook.com>
2022-03-31 23:01:45 +0200

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// Indexers and indexer overloading
+/// </summary>
+public class Car
+{
+    private static string[] _brands = new string[] {"Dacia", "Chevrolet", "Oldsmobile", "Cadillac"};
+    private  string[] _names = new string[] { "first", "second", "third", "fourth" };
+
+    private int length = _brands.Length;
+
+    //public indexer
+    public string this[int index]
+    {
+        set
+        {
+            _brands[index] = value;
+        }
+        get
+        {
+            return _brands[index]; 
+        }
+    }
+    public int Length {
+        get { return length; }
+    }
+
+    //indexer overloading
+    public string this[string name]
+    {
+        set
+        {
+            _brands[System.Array.IndexOf(_names, name)] = value;
+        }
+        get
+        {
+            return _brands[System.Array.IndexOf(_names, name)];
+        }
+
+    }
+}
---
+++ IndexersExample/Program.cs
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// In The Works : Indexers and indexer overloading
+/// 
+/// I cannot get the length of car instance unless I declare it static
+/// But being static, cannot create new instance for new values
+/// </summary> 
+class Program
+    {
+    static void Main()
+        {
+        Car car1 = new Car();
+        int numCars = car1.Length;
+        System.Console.WriteLine("There are " + numCars + " cars in inventory:");
+        System.Console.WriteLine();
+
+        for (int i = 0; i < numCars; i++)
+        {
+            System.Console.WriteLine(car1[i]);
+        }
+
+        System.Console.WriteLine("\nThere are " + numCars + " cars in inventory:");
+        System.Console.WriteLine();
+        car1[3] = "Renault";
+
+        for (int i = 0; i < numCars; i++)
+        {
+            System.Console.WriteLine(car1[i]);
+        }
+
+
+        Car car2 = new Car();
+        int numCars2 = car2.Length;
+        System.Console.WriteLine("\nThere are " + numCars + " cars in inventory listed using indexer overloading:");
+        System.Console.WriteLine();
+
+        System.Console.WriteLine(car2["first"]);
+        System.Console.WriteLine(car2["second"]);
+        System.Console.WriteLine(car2["third"]);
+        System.Console.WriteLine(car2["fourth"]);
+
+        System.Console.WriteLine("\nEnter any key to end the program.");
+        System.Console.ReadKey();
+
+    }
+}
+
